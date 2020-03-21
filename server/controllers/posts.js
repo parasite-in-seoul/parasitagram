e5e7835e177338da9590b3b07fff7d538f577022
Jayden <pyc8992dev@gmail.com>
2020-03-21 23:35:49 +0900

    post like list and comment like list added & post properties modified

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,13 +4,29 @@ exports.getPostsByUserId = async (req, res, next) => {
   try {
     const post = await db.Post.findAll({
       where: { UserId: req.params.id },
+      attributes: [['id', 'postNumber'], 'content', 'createdAt', 'updatedAt', ['userId', 'userNumber']],
       include: [{
         model: db.User,
-        attributes: ['id', 'nickname'],
+        through: 'like',
+        as: 'likers',
+        attributes: [['id', 'userNumber'], 'userId', 'nickName'],
+      }, {
+        model: db.Hashtag,
+        attributes: [['id', 'hashtagNumber'], 'name', 'createdAt', 'updatedAt'],
       }, {
         model: db.Image,
+        attributes: [['id', 'imageNumber'], 'src', 'createdAt', 'updatedAt'],
       }, {
         model: db.Comment,
+        attributes: [['id', 'commentNumber'], 'content', 'createdAt', 'updatedAt', 'userId', 'postId', ['commentId', 'parentId']],
+        include: [
+          {
+            model: db.User,
+            through: 'commentLike',
+            as: 'commentLikers',
+            attributes: [['id', 'userNumber'], 'userId', 'nickName'],
+          }
+        ]
       }],
     });
     res.json(post);
